fada13f8debe924246ee6f862f4eac890a8cda43
Mariusz <160026073+max4210@users.noreply.github.com>
2026-02-25 08:30:22 +0100

    cat9800 change config

--- terraform/config_inject.tf
+++ terraform/config_inject.tf
@@ -58,24 +58,35 @@ locals {
     local.auth_block
   ))
 
-  # WLC config: hostname + management IP (from data) + SSIDs
-  wlc_mgmt     = try(local.wlc_data.management, {})
-  wlc_mgmt_ip  = try(local.wlc_mgmt.ip, "10.0.3.1")
-  wlc_mgmt_mask = try(local.wlc_mgmt.mask, "255.255.255.0")
+  # WLC (C9800-CL) config: VLAN + management interface + wireless management interface
+  # C9800-CL uses interface vlan X for management, not IP on physical port
+  wlc_mgmt       = try(local.wlc_data.management, {})
+  wlc_mgmt_vlan  = try(local.wlc_mgmt.vlan, 20)
+  wlc_mgmt_ip    = try(local.wlc_mgmt.ip, "10.0.2.1")
+  wlc_mgmt_mask  = try(local.wlc_mgmt.mask, "255.255.255.0")
+  wlc_mgmt_gw    = try(local.wlc_mgmt.gateway, "10.0.2.2")
 
   wlc_config = join("\n", concat(
     [
       "hostname ${try(local.wlc_data.hostname, "WLC1")}",
       "no ip domain-lookup",
+      "vlan ${local.wlc_mgmt_vlan}",
+      " name management",
+      "interface GigabitEthernet0",
+      " switchport mode access",
+      " switchport access vlan ${local.wlc_mgmt_vlan}",
+      " no shutdown",
+      "interface Vlan${local.wlc_mgmt_vlan}",
+      " ip address ${local.wlc_mgmt_ip} ${local.wlc_mgmt_mask}",
+      " no shutdown",
+      "ip route 0.0.0.0 0.0.0.0 ${local.wlc_mgmt_gw}",
+      "wireless management interface Vlan${local.wlc_mgmt_vlan}",
+      "wireless country US",
+      "wireless config vwlc-ssc key-size 2048 signature-algo sha256 password 0 ${var.device_password}",
     ],
     flatten([for ssid in try(local.wlc_data.wireless.ssids, []) : [
       "! SSID: ${ssid.name} vlan ${try(ssid.vlan, 1)} ${try(ssid.security, "")}",
     ]]),
-    [
-      "interface GigabitEthernet0",
-      " ip address ${local.wlc_mgmt_ip} ${local.wlc_mgmt_mask}",
-      " no shutdown",
-    ],
     local.auth_block
   ))
 }
